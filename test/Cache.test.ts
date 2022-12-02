82ca4e19109584f60cdce84d45aa2ab55425e41d
qonfluent <zantrua+github@gmail.com>
2022-12-02 03:44:39 -0700

    Added more cache tests

--- test/Cache.test.ts
+++ test/Cache.test.ts
@@ -1,18 +1,26 @@
+import assert from "assert"
 import { CacheUnit } from "../src/Cache"
 
+const TEST_DATA_COUNT = 10
+
 describe('Cache', () => {
-	it('Can be read/write', () => {
-		const cache = new CacheUnit({
+	let cache: CacheUnit
+
+	const testData = [...Array(TEST_DATA_COUNT)].map((_, i) => new Uint8Array([i, Math.floor(Math.random() * 256), 0, 0]))
+
+	it('Can be created', () => {
+		cache = new CacheUnit({
 			widthBits: 32,
 			rowCount: 4,
 			ways: 1,
 		})
+	})
 
-		const data = new Uint8Array([1, 2, 3, 4])
+	it('Can be written to and read from', () => {
 		const write = cache.step({
 			write: true,
 			address: 0,
-			data,
+			data: testData[0],
 		})
 
 		expect(write).toEqual({ write: true })
@@ -22,6 +30,47 @@ describe('Cache', () => {
 			address: 0,
 		})
 
-		expect(read).toEqual({ write: false, data })
+		expect(read).toEqual({ write: false, data: testData[0] })
+	})
+
+	it('Can overwrite address data', () => {
+		const write = cache.step({
+			write: true,
+			address: 0,
+			data: testData[1],
+		})
+
+		expect(write).toEqual({ write: true })
+
+		const read = cache.step({
+			write: false,
+			address: 0,
+		})
+
+		expect(read).toEqual({ write: false, data: testData[1] })
+	})
+
+	it('Can evict data', () => {
+		const write = cache.step({
+			write: true,
+			address: 4,
+			data: testData[2],
+		})
+
+		expect(write).toEqual({ write: true, evicted: { address: 0, data: testData[1] } })
+
+		const failRead = cache.step({
+			write: false,
+			address: 0,
+		})
+
+		expect(failRead).toEqual({ write: false })
+
+		const read = cache.step({
+			write: false,
+			address: 4,
+		})
+
+		expect(read).toEqual({ write: false, data: testData[2] })
 	})
 })
