10421beaa20f1bcd5ae1eaf6fe503d761ad57c78
qonfluent <zantrua+github@gmail.com>
2023-02-09 16:21:29 -0700

    Transition to new parser combinator style

--- test/system/Parser.test.ts
+++ test/system/Parser.test.ts
@@ -1,9 +1,8 @@
 export type Symbol = string
 export type Text = string
-export type Body = Text | Command | Arg
-export type Arg = [string, Body[], string]
-export type Command = [Symbol, ...Arg[]]
-export type Document = (Command | Text)[]
+export type Arg = [number, string]
+export type Command = [Symbol, Arg[]]
+export type Document = [boolean, Command | Text][]
 
 export type ParserOptions = {
 	command: string
@@ -18,7 +17,178 @@ export type TextCursor = {
 	index: number
 }
 
-export class DocumentParser {
+export interface ICodec<A, B> {}
+
+export interface ICodecFactory {
+	alt<A, B>(...alts: ICodec<A, B>[]): ICodec<A, B>
+}
+
+
+export class EvalCodec<A, B> implements ICodec<A, B> {
+	public constructor(
+		public readonly encode: (a: A) => B,
+		public readonly decode: (b: B) => A,
+	) {}
+}
+
+export class EvalCodecRef<A, B> {
+	public constructor(
+		public readonly factory: ICodecFactory,
+		public readonly codec: EvalCodec<A, B>,
+	) {}
+}
+
+export class CodecFactory implements ICodecFactory {
+	public seq<A, B, C>(f: EvalCodec<A, B>, g: EvalCodec<B, C>): EvalCodec<A, C> {
+		return new EvalCodec(
+			(a) => g.encode(f.encode(a)),
+			(c) => f.decode(g.decode(c)),
+		)
+	}
+
+	public flip<A, B>(codec: EvalCodec<A, B>): EvalCodec<B, A> {
+		return new EvalCodec(codec.decode, codec.encode)
+	}
+
+	public alt<A, B, C, D>(left: EvalCodec<A, [B, D]>, right: EvalCodec<A, [C, D]>): EvalCodec<A, [[boolean, B | C], D]> {
+		return new EvalCodec(
+			(a): [[boolean, B | C], D] => {
+				try {
+					const [b, d] = left.encode(a)
+					return [[true, b], d]
+				} catch {
+					const [c, d] = right.encode(a)
+					return [[false, c], d]
+				}
+			},
+			([[isLeft, bOrC], d]) => (isLeft ? left : right).decode([bOrC as B & C, d]),
+		)
+	}
+
+	public altn<A, B>(...alts: EvalCodec<A, B>[]): EvalCodec<A, [number, B]> {
+		return new EvalCodec(
+			(a) => {
+				for (let i = 0; i < alts.length; i++) {
+					try {
+						return [i, alts[i].encode(a)]
+					} catch {}
+				}
+
+				throw new Error('No match')
+			},
+			([i, b]) => alts[i].decode(b),
+		)
+	}
+
+	// TODO: Semigroup instead of string
+	public repeat<B>(codec: EvalCodec<string, [B, string]>): EvalCodec<string, [B[], string]> {
+		return new EvalCodec(
+			(a) => {
+				const bs: B[] = []
+				while (true) {
+					try {
+						const [value, newA] = codec.encode(a)
+						if (newA === a) {
+							return [bs, a]
+						}
+
+						bs.push(value)
+						a = newA
+					} catch {
+						return [bs, a]
+					}
+				}
+			},
+			([bs, a]) => bs.reverse().reduce((a, b) => codec.decode([b, a]), a),
+		)
+	}
+
+	// FIXME: Semigroup and equality
+	public readUntilAny(until: string[]): EvalCodec<string, [string, string]> {
+		return new EvalCodec(
+			(a) => {
+				const index = until.reduce((index, u) => {
+					const i = a.indexOf(u)
+					return i === -1 ? index : i < index ? i : index
+				}, a.length)
+
+				return [a.slice(0, index), a.slice(index)]
+			},
+			([a, b]) => a + b,
+		)
+	}
+
+	// FIXME: Make this a general equality check on the prefix of a semigroup
+	public constText(text: string): EvalCodec<string, string> {
+		return new EvalCodec(
+			(a) => {
+				if (a.startsWith(text)) {
+					return a.slice(text.length)
+				}
+
+				throw new Error('No match')
+			},
+			(a) => text + a,
+		)
+	}
+
+	// FIXME: Internalise the predicate
+	public checkText(pred: (text: string) => boolean): EvalCodec<string, string> {
+		return new EvalCodec(
+			(a) => {
+				if (pred(a)) {
+					return a
+				}
+
+				throw new Error('No match')
+			},
+			(a) => a,
+		)
+	}
+
+	// FIXME: Use an internal type for this instead of { length: number }
+	public checkLength<T extends { length: number }>(pred: (length: number) => boolean): EvalCodec<T, T> {
+		return new EvalCodec(
+			(a) => {
+				if (pred(a.length)) {
+					return a
+				}
+
+				throw new Error('No match')
+			},
+			(a) => a,
+		)
+	}
+
+	public pair<A, B, C>(): EvalCodec<[A, [B, C]], [[A, B], C]> {
+		return new EvalCodec(
+			([a, [b, c]]) => [[a, b], c],
+			([[a, b], c]) => [a, [b, c]],
+		)
+	}
+
+	// TODO: All internal, this should be a function
+	public surround<A>(parens: [string, string][], codec: EvalCodec<string, [A, string]>): EvalCodec<string, [[number, A], string]> {
+		const terms = parens.map(([open, close]) => this.seq(this.constText(open), this.seq(codec, this.snd(this.constText(close)))))
+		return this.seq(this.altn(...terms), this.pair())
+	}
+
+	public fst<A, B, C>(codec: EvalCodec<A, B>): EvalCodec<[A, C], [B, C]> {
+		return new EvalCodec(
+			([a, c]) => [codec.encode(a), c],
+			([b, c]) => [codec.decode(b), c],
+		)
+	}
+
+	public snd<A, B, C>(codec: EvalCodec<A, B>): EvalCodec<[C, A], [C, B]> {
+		return new EvalCodec(
+			([c, a]) => [c, codec.encode(a)],
+			([c, b]) => [c, codec.decode(b)],
+		)
+	}
+}
+
+export class DocumentParser2 {
 	public constructor(
 		public readonly options: ParserOptions = {
 			command: '@',
@@ -29,251 +199,83 @@ export class DocumentParser {
 		},
 	) {}
 
-	// Document = (Command / Text)*
-	public textDoc(cursor: TextCursor): Document {
-		const doc: Document = []
-		while (cursor.index < cursor.text.length) {
-			const comment = this.textComment(cursor)
-			if (comment) {
-				doc.push(comment)
-				continue
-			}
-			
-			const command = this.textCommand(cursor)
-			if (command) {
-				doc.push(command)
-				continue
-			}
+	public textDoc(f: CodecFactory): EvalCodec<Text, [Document, Text]> {
+		const argBody = f.readUntilAny([this.options.command, ...this.options.parens.flat()])
+		const arg = f.surround(this.options.parens, argBody)
 
-			const text = this.textText(cursor)
-			if (text) {
-				doc.push(text)
-				continue
-			}
+		const symbol = f.readUntilAny([this.options.command, this.options.comment, ...this.options.newlines, ...this.options.spaces, ...this.options.parens.flat()])
+		const command = f.seq(f.constText(this.options.command), f.seq(f.seq(symbol, f.snd(f.repeat(arg))), f.pair()))
 
-			break
-		}
+		const text = f.readUntilAny([this.options.command])
+		
+		const segment = f.alt(command, text)
+		const document = f.repeat(segment)
 
-		return doc
-	}
-
-	// Text = [^@]+
-	public textText(cursor: TextCursor): Text | undefined {
-		const index = cursor.text.indexOf(this.options.command, cursor.index)
-		if (index === -1) {
-			const text = cursor.text.slice(cursor.index)
-			cursor.index = cursor.text.length
-			return text
-		}
-
-		const text = cursor.text.slice(cursor.index, index)
-		cursor.index = index
-		return text
-	}
-
-	// Comment = '@;' ![({[] [^\n\r]*
-	public textComment(cursor: TextCursor): Command | undefined {
-		// Check if it is a comment
-		if (!cursor.text.startsWith(this.options.command + this.options.comment, cursor.index)) {
-			return undefined
-		}
-
-		// Check if it is a single line comment
-		if (this.options.parens.some(([open]) => cursor.text.startsWith(open, cursor.index))) {
-			return undefined
-		}
-
-		// Skip the comment tag
-		cursor.index += this.options.command.length + this.options.comment.length
-
-		// Find the end of the comment
-		const index = Math.min(cursor.text.length, ...this.options.newlines.map((newline) => cursor.text.indexOf(newline, cursor.index)).filter((index) => index !== -1))
-
-		// Create the comment
-		const comment = cursor.text.slice(cursor.index, index)
-		cursor.index = index
-
-		return [this.options.comment, ['', [comment], '']]
-	}
-
-	// Command = '@' s:Symbol? a:Arg* ![({[]
-	public textCommand(cursor: TextCursor): Command | undefined {
-		const start = cursor.index
-
-		// Read command tag
-		if (!cursor.text.startsWith(this.options.command, cursor.index)) {
-			return undefined
-		}
-
-		cursor.index += this.options.command.length
-
-		// Read symbol
-		const symbol = this.textSymbol(cursor)
-
-		// Read args
-		const args: Arg[] = []
-		while (cursor.index < cursor.text.length) {
-			const arg = this.textArg(cursor)
-			if (arg) {
-				args.push(arg)
-				continue
-			}
-
-			break
-		}
-
-		// Make sure the command is not followed by an open paren
-		// NOTE: This is to prevent strange behavior when a command is followed by unbalanced parens
-		if (this.options.parens.some(([open]) => cursor.text.startsWith(open, cursor.index))) {
-			cursor.index = start
-			return undefined
-		}
-
-		return [symbol, ...args]
-	}
-
-	// Symbol = [^@ \t\n\r(){}\[\]]+
-	public textSymbol(cursor: TextCursor): Symbol {
-		const start = cursor.index
-		const stops = [this.options.command, ...this.options.spaces, ...this.options.newlines, ...this.options.parens.flat()]
-		const stopIndex = Math.min(cursor.text.length, ...stops.map((stop) => cursor.text.indexOf(stop, cursor.index)).filter((index) => index !== -1))
-		const symbol = cursor.text.slice(cursor.index, stopIndex)
-		cursor.index = stopIndex
-		return symbol
-	}
-
-	// Arg = &[({[] '(' Body ')' / '[' Body ']' / '{' Body '}'
-	public textArg(cursor: TextCursor): Arg | undefined {
-		const start = cursor.index
-
-		if (!this.options.parens.some(([open]) => cursor.text.startsWith(open, cursor.index))) {
-			return undefined
-		}
-
-		for (const [open, close] of this.options.parens) {
-			// Read open paren
-			if (!cursor.text.startsWith(open, cursor.index)) {
-				continue
-			}
-
-			cursor.index += open.length
-
-			// Read body
-			const body = this.textBody(cursor)
-
-			// Read close paren
-			if (!cursor.text.startsWith(close, cursor.index)) {
-				cursor.index = start
-				continue
-			}
-
-			cursor.index += close.length
-
-			return [open, body, close]
-		}
-
-		return undefined
-	}
-
-	// Body = body:($[^@(){}\[\]]+ / Command / Arg)*
-	public textBody(cursor: TextCursor): Body[] {
-		const body: Body[] = []
-		while (cursor.index < cursor.text.length) {
-			const text = this.textBodyText(cursor)
-			if (text) {
-				body.push(text)
-				continue
-			}
-
-			const command = this.textCommand(cursor)
-			if (command) {
-				body.push(command)
-				continue
-			}
-
-			const arg = this.textArg(cursor)
-			if (arg) {
-				body.push()
-				continue
-			}
-
-			break
-		}
-
-		return body
-	}
-
-	// Text = [^@(){}\[\]]+
-	public textBodyText(cursor: TextCursor): Text | undefined {
-		const stops = [this.options.command, ...this.options.parens.flat()]
-		const stopIndex = Math.min(cursor.text.length, ...stops.map((stop) => cursor.text.indexOf(stop, cursor.index)).filter((index) => index !== -1))
-		if (stopIndex === cursor.index) {
-			return undefined
-		}
-		const text = cursor.text.slice(cursor.index, stopIndex)
-		cursor.index = stopIndex
-		return text
+		return document
 	}
 }
 
-describe('Reference parser', () => {
-	it('Can parse an empty document', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '', index: 0 })
-		expect(doc).toEqual([])
+describe('Document parser', () => {
+	it('Can parse a blank document', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('')
+		expect(doc).toEqual([[], ''])
 	})
 
-	it('Can parse a document with a single command', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@foo', index: 0 })
-		expect(doc).toEqual([['foo']])
+	it('Can parse a document with normal text', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('Hello world!')
+		expect(doc).toEqual([[[false, 'Hello world!']], ''])
 	})
 
-	it('Can parse a document with a single command and a single arg', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@foo(bar)', index: 0 })
-		expect(doc).toEqual([['foo', ['(', ['bar'], ')']]])
+	it('Can print a document with normal text', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const text = textParser.decode([[[false, 'Hello world!']], ''])
+		expect(text).toEqual('Hello world!')
 	})
 
-	it('Can parse a document with a single command and a single arg with text before', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: 'hello @foo(bar)', index: 0 })
-		expect(doc).toEqual(['hello ', ['foo', ['(', ['bar'], ')']]])
+	it('Can parse a document with a command', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('@command')
+		expect(doc).toEqual([[[true, ['command', []]]], ''])
 	})
 
-	it('Can parse a document with a single command and a single arg with text after', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@foo(bar) hello', index: 0 })
-		expect(doc).toEqual([['foo', ['(', ['bar'], ')']], ' hello'])
+	it('Can parse a document with text and a command', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('Hello @command')
+		expect(doc).toEqual([[[false, 'Hello '], [true, ['command', []]]], ''])
 	})
 
-	it('Can parse a document with a single command and a single arg with text before and after', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: 'hello @foo(bar) goodbye', index: 0 })
-		expect(doc).toEqual(['hello ', ['foo', ['(', ['bar'], ')']], ' goodbye'])
+	it('Can parse a document with a command and text', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('@command Hello')
+		expect(doc).toEqual([[[true, ['command', []]], [false, ' Hello']], ''])
 	})
 
-	it('Can parse a comment', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@; comment\nnormal text', index: 0 })
-		expect(doc).toEqual([[';', ['', [' comment'], '']], '\nnormal text'])
+	it('Can parse a document with a command and a command', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('@command1@command2')
+		expect(doc).toEqual([[[true, ['command1', []]], [true, ['command2', []]]], ''])
 	})
 
-	it('Can parse multiline content', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@uwu(@foo(bar)\n@foo(bar))', index: 0 })
-		expect(doc).toEqual([['uwu', ['(', [['foo', ['(', ['bar'], ')']], '\n', ['foo', ['(', ['bar'], ')']]], ')']]])
+	it('Can parse arguments', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('@command(arg1)')
+		expect(doc).toEqual([[[true, ['command', [[0, 'arg1']]]]], ''])
 	})
 
-	it('Can parse multiple args', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@foo(bar)(baz)', index: 0 })
-		expect(doc).toEqual([['foo', ['(', ['bar'], ')'], ['(', ['baz'], ')']]])
-	})
-
-	it('Can parse multiple commands', () => {
-		const parser = new DocumentParser()
-		const doc = parser.textDoc({ text: '@foo(bar)@baz(baz)', index: 0 })
-		expect(doc).toEqual([['foo', ['(', ['bar'], ')']], ['baz', ['(', ['baz'], ')']]])
+	it('Can parse arguments with spaces', () => {
+		const parser = new DocumentParser2()
+		const textParser = parser.textDoc(new CodecFactory())
+		const doc = textParser.encode('@command( arg1 )')
+		expect(doc).toEqual([[[true, ['command', [[0, ' arg1 ']]]]], ''])
 	})
 })
