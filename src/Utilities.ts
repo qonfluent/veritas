a544735a923f317f913f2eaa1ba88a2eee52f6e7
qonfluent <zantrua+github@gmail.com>
2023-03-27 04:26:40 -0700

    Move up to V8

--- src/Utilities.ts
+++
@@ -1,7 +0,0 @@
-export function rangeMap<T>(count: number, f: (i: number) => T): T[] {
-	return [...Array(count)].map((_, i) => f(i))
-}
-
-export function remove<T>(arr: T[], i: number): T[] {
-	return arr.slice(0, i).concat(arr.slice(i + 1))
-}
