15ca214a7aebd2d00096d03f8bd71c9035b49e09
qonfluent <zantrua+github@gmail.com>
2023-05-29 02:12:14 -0700

    Flesh out keep alive service

--- src/network/services/KeepAliveService.ts
+++ src/network/services/KeepAliveService.ts
@@ -1,6 +1,19 @@
+import { Ref, TimeDuration, TimeInstant } from '../../Utility'
+import { IConnection } from '../Connection'
 import { IService } from '../Service'
 
 export type PingMessage = {}
 export type PongMessage = {}
 
-export interface IKeepAliveService extends IService {}
+export type KeepAliveStats = {
+	// The time the last ping was sent
+	lastPing: TimeInstant
+
+	// The time the last pong was received
+	lastPong: TimeInstant
+}
+
+export interface IKeepAliveService extends IService {
+	get timeout(): TimeDuration
+	stat(connection: Ref<IConnection>): KeepAliveStats
+}
